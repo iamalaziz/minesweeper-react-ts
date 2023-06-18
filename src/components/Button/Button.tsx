c72dd355b6e4476e296bfc132e22dd5524efa0d7
iamalaziz <mashrabovabdulaziz2463@gmail.com>
2023-06-18 19:31:55 +0900

    -f generating cell values

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { CellState, CellValue } from '../../types';
+import SvgTemplate from '../SvgTemplate';
 
 interface ButtonProps {
   col: number;
@@ -8,9 +9,20 @@ interface ButtonProps {
   value: CellValue;
 }
 const Button: React.FC<ButtonProps> = ({ row, col, state, value }) => {
+  const renderContent = (): React.ReactNode => {
+    if (state === CellState.visible) {
+      if (value === CellValue.bomb) {
+        return <SvgTemplate name="bomb" />;
+      } else if (value !== CellValue.none) {
+        return value;
+      }
+    } else if (state === CellState.flagged) {
+      return <SvgTemplate name="flag" />;
+    }
+  };
   return (
-    <div className={`Button ${state === CellState.visible ? 'visible' : ''}`}>
-      {CellState.visible && value}
+    <div className={`Button ${state === CellState.visible ? 'visible' : ''} value-${value}`}>
+      {renderContent()}
     </div>
   );
 };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -10,7 +10,7 @@ export const generateCells = (): Cell[][] => {
     for (let col = 0; col < MAX_COLS; col++) {
       cells[row].push({
         value: CellValue.none,
-        state: CellState.visible,   // TODO: Make this open !!!
+        state: CellState.open,
       });
     }
   }
@@ -32,6 +32,72 @@ export const generateCells = (): Cell[][] => {
         })
       );
       bombsPlaced++;
+    }
+  }
+
+  // calculate the numbers for each cell
+
+  for (let rowIndex = 0; rowIndex < MAX_ROWS; rowIndex++) {
+    for (let colIndex = 0; colIndex < MAX_COLS; colIndex++) {
+      const currentCell = cells[rowIndex][colIndex];
+      if (currentCell.value === CellValue.bomb) {
+        continue;
+      }
+
+      let numberOfBombs = 0;
+
+      const topLeftBomb =
+        rowIndex > 0 && colIndex > 0 ? cells[rowIndex - 1][colIndex - 1] : null;
+      const topBomb = rowIndex > 0 ? cells[rowIndex - 1][colIndex] : null;
+      const topRightBomb =
+        rowIndex > 0 && colIndex < MAX_COLS - 1
+          ? cells[rowIndex - 1][colIndex + 1]
+          : null;
+      const leftBomb = colIndex > 0 ? cells[rowIndex][colIndex - 1] : null;
+      const rightBomb =
+        colIndex < MAX_COLS - 1 ? cells[rowIndex][colIndex + 1] : null;
+      const bottomLeftBomb =
+        rowIndex < MAX_ROWS - 1 && colIndex > 0
+          ? cells[rowIndex + 1][colIndex - 1]
+          : null;
+      const bottomBomb =
+        rowIndex < MAX_ROWS - 1 ? cells[rowIndex + 1][colIndex] : null;
+      const bottomRightBomb =
+        rowIndex < MAX_ROWS - 1 && colIndex < MAX_COLS - 1
+          ? cells[rowIndex + 1][colIndex + 1]
+          : null;
+
+      if (topLeftBomb?.value === CellValue.bomb) {
+        numberOfBombs++;
+      }
+      if (topBomb?.value === CellValue.bomb) {
+        numberOfBombs++;
+      }
+      if (topRightBomb?.value === CellValue.bomb) {
+        numberOfBombs++;
+      }
+      if (rightBomb?.value === CellValue.bomb) {
+        numberOfBombs++;
+      }
+      if (leftBomb?.value === CellValue.bomb) {
+        numberOfBombs++;
+      }
+      if (bottomLeftBomb?.value === CellValue.bomb) {
+        numberOfBombs++;
+      }
+      if (bottomBomb?.value === CellValue.bomb) {
+        numberOfBombs++;
+      }
+      if (bottomRightBomb?.value === CellValue.bomb) {
+        numberOfBombs++;
+      }
+
+      if (numberOfBombs > 0) {
+        cells[rowIndex][colIndex] = {
+          ...currentCell,
+          value: numberOfBombs,
+        };
+      }
     }
   }
 
