a7498a06f4ca211cbd73ba319533741f4cb740f5
iamalaziz <mashrabovabdulaziz2463@gmail.com>
2024-01-28 23:57:59 +0900

    setting up store for state management

---
+++ src/store/timeSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  seconds: 0,
+};
+export const timerSlice = createSlice({
+  name: 'timer',
+  initialState,
+  reducers: {
+    startTimer: (state) => {
+      const timer = setInterval(() => {
+        state.seconds += 1;
+      }, 1000);
+      return () => {
+        clearInterval(timer);
+      };
+    },
+  },
+});
+
+export const {startTimer} = timerSlice.actions;
+export default timerSlice.reducer
